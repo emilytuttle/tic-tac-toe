abdc3d33b951f3244d0d793f295eb09ea7e26efe
Emily Tuttle <emilytuttle00@gmail.com>
2022-09-23 09:55:10 -0600

    all steps done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import Square from './components/Square.js'
 import React, { useState } from 'react';
 
 function App() {
+  const calculateWinner = array => {
+    let lines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],[2,5,8], [0,4,8], [2,4,6]]
+    for (let i=0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+        if (
+          array[a] &&
+          array[a] === array[b] &&
+          array[a] === array[c]
+      ) {
+          return `${array[a]} won!`;
+      }
+    }
+    return "Who will win?"
+  }
   const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', ''])
   const [player, setPlayer] = useState(true);
 
@@ -16,7 +30,7 @@ function App() {
       <div className="container">
         {squares.map((value, index) => {
           return (
-            <Square 
+              <Square 
               squares={squares} 
               setSquares={setSquares}
               player={player}
@@ -26,6 +40,7 @@ function App() {
             /> 
           )
         })}
+        <span>{calculateWinner(squares)}</span>
       </div>
       <button onClick={handleClick}>Reset</button>
     </div>
